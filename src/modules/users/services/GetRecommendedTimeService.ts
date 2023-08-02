0f355c88aae6801f45d43eed69c93a78a3c99255
Luiz <you@example.com>
2023-08-02 10:18:15 -0300

    :bug:

--- src/modules/users/services/GetRecommendedTimeService.ts
+++ src/modules/users/services/GetRecommendedTimeService.ts
@@ -38,7 +38,7 @@ export default class GetCalendarEvents {
 
     const schedule = await urlservice.authenticate(phone);
     const horarios:calendar_v3.Schema$Event[] = [];
-    schedule.forEach((element) => {
+    await schedule.forEach((element) => {
       horarios.push(element);
     });
 
@@ -51,7 +51,7 @@ export default class GetCalendarEvents {
       }
     }
     // eslint-disable-next-line no-sequences
-    const simplerS = horarios.map((event) => ([moment(event.start?.dateTime), moment(event.end?.dateTime)]));
+    const simplerS = await horarios.map((event) => ([moment(event.start?.dateTime), moment(event.end?.dateTime)]));
 
     if (simplerS === undefined) throw new AppError('Uasdasda', 400);
 
@@ -70,51 +70,48 @@ export default class GetCalendarEvents {
 
     // Sort the array based on the first datetime of each index
 
-    data.sort(compareDates);
-    try {
-      // eslint-disable-next-line consistent-return
+    await data.sort(compareDates);
 
-      // eslint-disable-next-line consistent-return
-      data.forEach((scheduleSet, index) => {
-        if (index + 1 > data.length) {
-          return freeTimes;
-        }
-        if (data[index + 1] !== undefined || scheduleSet !== undefined) {
-          if (scheduleSet[1] !== undefined || data[index + 1][0] !== undefined) {
-            const start = moment(scheduleSet[1]);
+    data.forEach((scheduleSet, index) => {
+      if (index + 1 > data.length) {
+        return freeTimes;
+      }
+      if (data[index + 1] !== undefined || scheduleSet !== undefined) {
+        if (scheduleSet[1] !== undefined || data[index + 1][0] !== undefined) {
+          const start = moment(scheduleSet[1]);
 
-            const end = moment(data[index + 1][0]);
+          const end = moment(data[index + 1][0]);
 
-            const diff = (Date.parse(end.toString()) - Date.parse(start.toString())) / 60000;
+          const diff = (Date.parse(end.toString()) - Date.parse(start.toString())) / 60000;
 
-            if (start < end && start > moment(beginDate) && end < moment(endDate).add(1, 'days') && duration <= diff) {
-              const startDate1 = moment(start);
-              startDate1.set('hour', parseInt(beginHour.slice(0, 2), 10));
-              startDate1.set('minute', parseInt(beginHour.slice(3, 5), 10));
-              startDate1.set('seconds', parseInt(beginHour.slice(6, 8), 10));
+          if (start < end && start > moment(beginDate) && end < moment(endDate).add(1, 'days') && duration <= diff) {
+            const startDate1 = moment(start);
+            startDate1.set('hour', parseInt(beginHour.slice(0, 2), 10));
+            startDate1.set('minute', parseInt(beginHour.slice(3, 5), 10));
+            startDate1.set('seconds', parseInt(beginHour.slice(6, 8), 10));
 
-              const endDate1 = moment(start);
-              endDate1.set('hour', parseInt(endHour.slice(0, 2), 10));
-              endDate1.set('minute', parseInt(endHour.slice(3, 5), 10));
-              endDate1.set('seconds', parseInt(endHour.slice(6, 8), 10));
+            const endDate1 = moment(start);
+            endDate1.set('hour', parseInt(endHour.slice(0, 2), 10));
+            endDate1.set('minute', parseInt(endHour.slice(3, 5), 10));
+            endDate1.set('seconds', parseInt(endHour.slice(6, 8), 10));
 
-              if (start > startDate1 && end < endDate1) {
-                let aux1 = start.clone();
-                const aux2 = start.clone();
+            if (start > startDate1 && end < endDate1) {
+              let aux1 = start.clone();
+              const aux2 = start.clone();
 
-                while (aux2 < end) {
-                  aux2.add(duration, 'minute');
+              while (aux2 < end) {
+                aux2.add(duration, 'minute');
 
-                  freeTimes.push({ start1: aux1.format(), end1: aux2.format() });
+                freeTimes.push({ start1: aux1.format(), end1: aux2.format() });
 
-                  aux1 = aux2.clone();
-                }
+                aux1 = aux2.clone();
               }
             }
           }
         }
-      });
-    } catch (e) { console.log(e); }
+      }
+      return freeTimes;
+    });
 
     return freeTimes;
   }
