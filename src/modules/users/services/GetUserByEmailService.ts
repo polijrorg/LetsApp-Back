52b7caec1ae3cdda99c9af83887b112f1f1f0c11
Luiz <you@example.com>
2023-08-24 12:09:15 -0300

    in construction

--- src/modules/users/services/GetUserByEmailService.ts
+++ src/modules/users/services/GetUserByEmailService.ts
@@ -15,8 +15,8 @@ export default class GetUserByEmailService {
 
   ) { }
 
-  public async execute(phone:string): Promise<IUsersVerified> {
-    const user = await this.usersRepository.findByPhone(phone);
+  public async execute(email:string): Promise<IUsersVerified> {
+    const user = await this.usersRepository.findByEmail(email);
     if (!user) throw new AppError('User Not Found', 400);
     try {
       const response = await axios.get(`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${user.tokens}`);
