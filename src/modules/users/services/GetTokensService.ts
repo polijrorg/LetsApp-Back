158750dea0c28776022c18b6501cd21c921efffd
Danteo <contasntec@polijunior.com.br>
2023-08-28 10:31:27 -0300

    Add/getToken

--- src/modules/users/services/GetTokensService.ts
+++ src/modules/users/services/GetTokensService.ts
@@ -1,4 +1,4 @@
-import { google } from 'googleapis';
+import msal from '@azure/msal-node';
 import { inject, injectable } from 'tsyringe';
 import AppError from '@shared/errors/AppError';
 import IUsersRepository from '../repositories/IUsersRepository';
@@ -11,20 +11,51 @@ export default class GetTokensService {
   ) { }
 
   public async authenticate(code: string): Promise<void> {
-    const oAuth2Client = new google.auth.OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.CLIENT_URI);
-    const { tokens } = await oAuth2Client.getToken(code);
+    // const oAuth2Client = new google.auth.OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.CLIENT_URI);
+    // const { tokens } = await oAuth2Client.getToken(code);
 
-    oAuth2Client.setCredentials(tokens);
+    // oAuth2Client.setCredentials(tokens);
 
-    const userInfo = await google.oauth2('v2').userinfo.get({ auth: oAuth2Client });
-    if (!userInfo.data.email) {
-      throw new AppError('Email not found in user info', 400);
-    }
+    // const userInfo = await google.oauth2('v2').userinfo.get({ auth: oAuth2Client });
+    // if (!userInfo.data.email) {
+    //   throw new AppError('Email not found in user info', 400);
+    // }
 
-    const user = await this.usersRepository.findByEmail(userInfo.data.email);
+    // const user = await this.usersRepository.findByEmail(userInfo.data.email);
+    // if (!user) throw new AppError('User not found', 400);
+    // if (!tokens.access_token) throw new AppError('Token not found', 400);
+
+    // this.usersRepository.updateToken(user.id, tokens.access_token);
+    const clientConfig = {
+      auth: {
+        clientId: process.env.CLIENT_ID as string,
+        clientSecret: process.env.CLIENT_SECRET,
+      },
+    };
+
+    const cca = new msal.ConfidentialClientApplication(clientConfig);
+
+    const tokenRequest = {
+      code,
+      redirectUri: process.env.CLIENT_URI as string,
+      scopes: ['https://graph.microsoft.com/.default'],
+    };
+
+    const tokens = await cca.acquireTokenByCode(tokenRequest);
+
+    const response = await fetch('https://graph.microsoft.com/v1.0/me', {
+      headers: {
+        Authorization: `Bearer ${tokens.accessToken}`,
+      },
+    });
+
+    const userInfo = await response.json();
+
+    // Unificar??
+    const user = await this.usersRepository.findByEmail(userInfo.mail);
     if (!user) throw new AppError('User not found', 400);
-    if (!tokens.access_token) throw new AppError('Token not found', 400);
+    if (!tokens.accessToken) throw new AppError('Token not found', 400);
 
-    this.usersRepository.updateToken(user.id, tokens.access_token);
+    this.usersRepository.updateToken(user.id, tokens.accessToken);
   }
 }
--- src/modules/users/services/GoogleAuthUrlService.ts
+++ src/modules/users/services/GoogleAuthUrlService.ts
@@ -1,4 +1,4 @@
-import { google } from 'googleapis';
+import msal from '@azure/msal-node';
 import { injectable, inject } from 'tsyringe';
 import IUsersRepository from '../repositories/IUsersRepository';
 
@@ -11,23 +11,39 @@ export default class GoogleAuthUrlService {
   ) { }
 
   public async authenticate(): Promise<string> {
-    const oauth2Client = new google.auth.OAuth2();
+    // const oauth2Client = new google.auth.OAuth2();
 
-    const scopes = ['https://www.googleapis.com/auth/calendar',
-      'https://www.googleapis.com/auth/calendar.events',
-      'https://www.googleapis.com/auth/calendar.events.readonly',
-      'https://www.googleapis.com/auth/calendar.readonly',
-      'https://www.googleapis.com/auth/calendar.settings.readonly',
-      'https://www.googleapis.com/auth/userinfo.email'];
+    // const scopes = ['https://www.googleapis.com/auth/calendar',
+    //   'https://www.googleapis.com/auth/calendar.events',
+    //   'https://www.googleapis.com/auth/calendar.events.readonly',
+    //   'https://www.googleapis.com/auth/calendar.readonly',
+    //   'https://www.googleapis.com/auth/calendar.settings.readonly',
+    //   'https://www.googleapis.com/auth/userinfo.email'];
 
-    const authUrl = oauth2Client.generateAuthUrl({
-      access_type: 'offline',
-      scope: scopes,
-      client_id: process.env.CLIENT_ID,
-      redirect_uri: process.env.CLIENT_URI,
+    // const authUrl = oauth2Client.generateAuthUrl({
+    //   access_type: 'offline',
+    //   scope: scopes,
+    //   client_id: process.env.CLIENT_ID,
+    //   redirect_uri: process.env.CLIENT_URI,
 
-    });
+    // });
 
-    return authUrl;
+    // return authUrl;
+    const clientConfig = {
+      auth: {
+        clientId: process.env.CLIENT_ID as string,
+        clientSecret: process.env.CLIENT_SECRET,
+      },
+    };
+
+    const cca = new msal.ConfidentialClientApplication(clientConfig);
+
+    const authCodeUrlParameters = {
+      scopes: ['user.read', 'calendars.read', 'calendars.readwrite', 'calendars.read.shared', 'calendars.readwrite.shared'],
+      redirectUri: process.env.CLIENT_URI as string,
+    };
+
+    const url = cca.getAuthCodeUrl(authCodeUrlParameters);
+    return url;
   }
 }
