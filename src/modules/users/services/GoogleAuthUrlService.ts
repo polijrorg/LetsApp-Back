c040918d4728e2d68ca6f32a3573b3faa1925cef
Danteo <contasntec@polijunior.com.br>
2023-08-29 15:10:11 -0300

    Add/outlookAuthentication

--- src/modules/users/services/GoogleAuthUrlService.ts
+++ src/modules/users/services/GoogleAuthUrlService.ts
@@ -1,4 +1,4 @@
-import msal from '@azure/msal-node';
+import { google } from 'googleapis';
 import { injectable, inject } from 'tsyringe';
 import IUsersRepository from '../repositories/IUsersRepository';
 
@@ -11,21 +11,23 @@ export default class GoogleAuthUrlService {
   ) { }
 
   public async authenticate(): Promise<string> {
-    const clientConfig = {
-      auth: {
-        clientId: process.env.CLIENT_ID as string,
-        clientSecret: process.env.CLIENT_SECRET,
-      },
-    };
+    const oauth2Client = new google.auth.OAuth2();
 
-    const cca = new msal.ConfidentialClientApplication(clientConfig);
+    const scopes = ['https://www.googleapis.com/auth/calendar',
+      'https://www.googleapis.com/auth/calendar.events',
+      'https://www.googleapis.com/auth/calendar.events.readonly',
+      'https://www.googleapis.com/auth/calendar.readonly',
+      'https://www.googleapis.com/auth/calendar.settings.readonly',
+      'https://www.googleapis.com/auth/userinfo.email'];
 
-    const authCodeUrlParameters = {
-      scopes: ['https://graph.microsoft.com/.default'],
-      redirectUri: process.env.CLIENT_URI as string,
-    };
+    const authUrl = oauth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: scopes,
+      client_id: process.env.CLIENT_ID,
+      redirect_uri: process.env.CLIENT_URI,
 
-    const authUrl = cca.getAuthCodeUrl(authCodeUrlParameters);
+    });
+
     return authUrl;
   }
 }
