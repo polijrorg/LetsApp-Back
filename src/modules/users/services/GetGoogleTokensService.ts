c934ca17b9883daa536da8adcb0064bde0e0c3b2
Nicholas Sasaki Ogata <nicholas.ogata@usp.br>
2023-12-05 14:30:19 -0300

    add error message

--- src/modules/users/services/GetGoogleTokensService.ts
+++ src/modules/users/services/GetGoogleTokensService.ts
@@ -10,7 +10,7 @@ export default class GetTokensService {
     private usersRepository: IUsersRepository,
   ) { }
 
-  public async authenticate(code: string, state: string): Promise<void> {
+  public async authenticate(code: string, state: string): Promise<boolean> {
     const oAuth2Client = new google.auth.OAuth2(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, process.env.GOOGLE_CLIENT_URI);
     const { tokens } = await oAuth2Client.getToken(code);
 
@@ -21,14 +21,20 @@ export default class GetTokensService {
       throw new AppError('Email not found in user info', 400);
     }
 
-    const user = await this.usersRepository.findByPhone(state);
-    if (!user) throw new AppError('User Not Found', 400);
-    await this.usersRepository.updateEmail(user?.id, userInfo.data.email);
+    try {
+      const user = await this.usersRepository.findByPhone(state);
+      if (!user) throw new AppError('User Not Found', 400);
+      await this.usersRepository.updateEmail(user?.id, userInfo.data.email);
 
-    if (!tokens.access_token) throw new AppError('Token not found', 400);
+      if (!tokens.access_token) throw new AppError('Token not found', 400);
 
-    this.usersRepository.updateToken(user.id, tokens.access_token);
+      this.usersRepository.updateToken(user.id, tokens.access_token);
 
-    this.usersRepository.updateUserType(user.id, 'GOOGLE');
+      this.usersRepository.updateUserType(user.id, 'GOOGLE');
+
+      return true;
+    } catch (error) {
+      return false;
+    }
   }
 }
